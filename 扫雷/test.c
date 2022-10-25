e829c9b8e8c5665b230a053e09d62a1ae7a1bd4f
Amon-peng <116494589+Amon-peng@users.noreply.github.com>
2022-10-25 21:10:11 +0800

    扫雷小游戏

---
+++ 扫雷/test.c
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"game.h"
+
+
+void menu()
+{
+	printf("****************************\n");
+	printf("****1.play********0.exit****\n");
+	printf("****************************\n");
+}
+
+void game()
+{
+	char mine[ROWS][COLS] = { 0 };//布置好雷的信息
+	char show[ROWS][COLS] = { 0 };//排查出雷的信息
+	InitBoard(mine, ROWS, COLS, '0');//初始化
+	InitBoard(show, ROWS, COLS, '*');
+	//DisplayBoard(mine, ROW, COL);
+	DisplayBoard(show, ROW, COL);//打印棋盘
+	SetMine(mine, ROW, COL);//布置雷
+	//DisplayBoard(mine, ROW, COL);
+	FindMine(mine, show, ROW, COL);//扫雷
+}
+
+void test()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do
+	{
+		menu();
+		printf("请选择：");
+		scanf_s("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("选择错误，重新选择！\n");
+			break;
+		}
+	} while (input);
+}
+
+int main()
+{
+	test();
+	return 0;
+}
